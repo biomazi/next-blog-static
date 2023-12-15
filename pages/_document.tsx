6032199c60aa0ef4cf3b706d341d42560cd947be
Marko Mazibrada <biomazi@gmail.com>
2023-12-15 19:01:33 +0100

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <meta name='Marko i Damir' />
       <Head />
       <body>
         <Main />
